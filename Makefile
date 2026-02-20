57c1abe386f31183a247635339fe1b7eab83cde4
Matteo Croce <teknoraver@meta.com>
2026-02-20 15:15:44 +0100

    suport many functions

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 CFLAGS := -O2 -pipe -Wall
 ASFLAGS := $(CFLAGS) -march=rv64gv
 
-testmemcpy: main.o memcpy.o
+memcpys := memcpy_64x10.o
+
+testmemcpy: main.o $(memcpys)
 	$(CC) ${CFLAGS} $^ -o $@
 
 test:: testmemcpy
--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <time.h>
 
-extern void *__memcpy(void *, const void *, size_t);
+extern void *memcpy_64x10(void *, const void *, size_t);
+
+typedef void *(memcpy_t)(void *, const void *, size_t);
+
+static struct memcpy_func {
+	const char *name;
+	memcpy_t *func;
+} memcpys[] = {
+	{ "memcpy", memcpy },
+	{ "memcpy_64x10", memcpy_64x10 },
+};
 
 static int err;
 
-int test(char *buf1, char *buf2, size_t size)
+static int test(memcpy_t func, char *buf1, char *buf2, size_t size)
 {
 	for (int i = 0; i < size; i++)
 		buf1[i] = random();
 
-	__memcpy(buf2, buf1, size);
+	func(buf2, buf1, size);
 	
 	if (memcmp(buf1, buf2, size)) {
 		err = 1;
@@ -23,26 +34,81 @@ int test(char *buf1, char *buf2, size_t size)
 	return 0;
 }
 
-int main(int argc, char *argv[])
+static int tests(memcpy_t func)
 {
 	char buf1[256], buf2[256];
 
 	srandom(time(NULL));
 
-	test(buf1, buf2, 0);
+	test(func, buf1, buf2, 0);
 
-	test(buf1, buf2, sizeof(buf1));
-	test(buf1, buf2, 16);
-	test(buf1, buf2, 3);
+	test(func, buf1, buf2, sizeof(buf1));
+	test(func, buf1, buf2, 16);
+	test(func, buf1, buf2, 3);
 
-	test(buf1+1, buf2, 200);
-	test(buf1+1, buf2+1, 200);
-	test(buf1, buf2+1, 200);
-	test(buf1, buf2+5, 200);
-	test(buf1+2, buf2+4, 200);
+	test(func, buf1+1, buf2, 200);
+	test(func, buf1+1, buf2+1, 200);
+	test(func, buf1, buf2+1, 200);
+	test(func, buf1, buf2+5, 200);
+	test(func, buf1+2, buf2+4, 200);
 
-	test(buf1, buf2+1, 40);
-	test(buf1+3, buf2+1, 40);
+	test(func, buf1, buf2+1, 40);
+	test(func, buf1+3, buf2+1, 40);
 
 	return err;
+}
+
+#define BENCH_SIZE (128 * 1024 * 1024)
+#define LOOPS 10
+
+int bench(memcpy_t func)
+{
+	char *src = malloc(BENCH_SIZE);
+	char *dst = malloc(BENCH_SIZE);
+	struct timespec start, end;
+	uint64_t total_ns = 0;
+
+	for (int i = 0; i < BENCH_SIZE; i++)
+		src[i] = dst[i] = i;
+
+	for (int i = 0; i < LOOPS; i++) {
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		func(dst, src, BENCH_SIZE);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+
+		// Prevent the compiler from optimizing away the memcpy.
+		asm volatile("" :: "m"(*(char(*)[BENCH_SIZE])dst));
+		total_ns += (end.tv_sec - start.tv_sec) * 1000000000L
+			  + (end.tv_nsec - start.tv_nsec);
+	}
+
+	free(src);
+	free(dst);
+
+	double secs = total_ns / 1e9;
+	double mb = (double)BENCH_SIZE * LOOPS / (1024.0 * 1024.0);
+	printf("%.2f MB/s\n", mb / secs);
+
+	return 0;
+}
+
+int test_func(memcpy_t func)
+{
+	if (tests(func))
+		return 1;
+
+	bench(func);
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	for (size_t i = 0; i < sizeof(memcpys) / sizeof(memcpys[0]); i++) {
+		printf("Testing %s...\n", memcpys[i].name);
+		if (test_func(memcpys[i].func))
+			return 1;
+	}
+
+	return 0;
 }
